afc08831698ee29ffe17dda407c92e8876edb70a
Cody Lundquist <cody@getfeedback.com>
2020-04-05 13:31:58 -0700

    Not my best work, but this is good for now.

--- htmlbars-loader.js
+++ htmlbars-loader.js
@@ -6,6 +6,9 @@ var DEFAULT_TEMPLATE_COMPILER = "components-ember/ember-template-compiler.js";
 var templateTree;
 var appPath;
 var templatesFolder;
+var templateCompiler;
+var newDeprecationSet = false;
+var deprecationWorkflow;
 
 /*
  * options:
@@ -15,19 +18,81 @@ var templatesFolder;
 module.exports = function (source) {
   this.cacheable && this.cacheable();
   var options = loaderUtils.getOptions(this) || {};
+  var resourcePath = this.resourcePath;
+  deprecationWorkflow =
+    deprecationWorkflow || options.deprecationWorkflow || [];
   appPath = (options.appPath || "app").replace(/\/$/, "");
   templatesFolder = templatesFolder || path.join(appPath, "templates");
+  templateCompiler =
+    templateCompiler ||
+    require(options.templateCompiler || DEFAULT_TEMPLATE_COMPILER);
 
   templateTree =
     templateTree ||
     new HtmlbarsCompiler(templatesFolder, {
       isHTMLBars: true,
-      templateCompiler: require(options.templateCompiler ||
-        DEFAULT_TEMPLATE_COMPILER),
+      templateCompiler,
     });
 
+  if (!newDeprecationSet) {
+    newDeprecationSet = true;
+    var Ember = templateCompiler._Ember;
+    const originalDeprecate = Ember.deprecate;
+    Ember.deprecate = function (message, test, options) {
+      var noDeprecation;
+
+      if (typeof test === "function") {
+        noDeprecation = test();
+      } else {
+        noDeprecation = test;
+      }
+
+      let foundDeprecationHandler = false;
+
+      if (!noDeprecation) {
+        let i = -1;
+        while (++i < deprecationWorkflow.length) {
+          let wf = deprecationWorkflow[i];
+          if (
+            (options && wf.matchId && options.id === wf.matchId) ||
+            (wf.matchMessage &&
+              wf.matchMessage.test &&
+              wf.matchMessage.test(message)) ||
+            wf.matchMessage === message
+          ) {
+            switch (wf.handler || "log") {
+              case "log":
+                console.log(
+                  "DEPRECATION: Message:",
+                  message,
+                  "Options:",
+                  options,
+                  "File:",
+                  resourcePath
+                );
+                break;
+              case "throw":
+                throw new Error(message);
+              case "silence":
+                break;
+            }
+
+            foundDeprecationHandler = true;
+            break;
+          }
+        }
+      }
+
+      if (!foundDeprecationHandler) {
+        return originalDeprecate.apply(this, arguments);
+      }
+
+      return options;
+    };
+  }
+
   var resourcePathMatcher = new RegExp(`${templatesFolder}/(.*)\\.[^.]+$`);
-  var templateMatch = this.resourcePath.match(resourcePathMatcher);
+  var templateMatch = resourcePath.match(resourcePathMatcher);
   var templatePath = templateMatch.pop();
 
   var fullTemplate = templateTree.processString(source, templatePath);
